9da01b118c40e6885c3d9d7e91590c12f999c6bc
Giwan Lim <march.22nd.hare@gmail.com>
2025-05-11 11:38:29 +0900

    오탈자 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>자판기기</h1>
+        <h1>자판기</h1>
       </header>
       <main>
         <VendingMachine />
